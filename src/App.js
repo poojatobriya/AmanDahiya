a9bc0a1c9e80ce338179ca527c8760b6733744c5
Aman <Dr.AmanNavodian@gmail.com>
2024-04-18 23:10:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React ,{useState} from 'react'
+import Navbar from './components/Navbar'
+import Form from './components/Form'
+// import About from './components/About'
 
-function App() {
+export default function App() {
+  const [mode,setmode]=useState('light');
+
+  const togglemode=()=>{
+    if(mode==='light'){
+      setmode('dark');
+      document.body.style.backgroundColor='#042743';
+    }
+    else{
+      setmode('light');
+      document.body.style.backgroundColor='white';
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Navbar title="aman" aboutText="dahiya" mode={mode} togglemode={togglemode} />
+      <div className="container my-3">
+            <Form heading="Enter text to analyze" mode={mode}/>
+            {/* <About/> */}
+      </div>
+     
+    </>
+  )
 }
-
-export default App;
---
+++ src/components/Form.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import {useState} from 'react'
+
+
+
+export default function Form(props) {
+  const handleonchange=(event)=>{
+                  setText(event.target.value);
+  }
+  const handleupclick=()=>{
+    setText(text.toUpperCase());
+  }
+  const handleloclick=()=>{
+    setText(text.toLowerCase());
+  }
+  const handleclrclick=()=>{
+    let newtext="";
+    setText(newtext);
+  }
+  const [text,setText]=useState('');
+  return (
+    <>
+    <div className="container" style={{color: props.mode==='dark'?'white':'#042743'}}>
+      <h1>{props.heading}</h1>
+
+      <div className="mb-3">
+        <textarea className="form-control" id="myBox" value={text} onChange={handleonchange} style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'#042743'}} rows="8"></textarea>
+      </div>
+      
+     <button className="btn btn-primary mx-2" onClick={handleupclick}>convert to uppercase</button>
+     <button className="btn btn-primary mx-2" onClick={handleloclick}>convert to lowercase</button>
+     <button className="btn btn-primary mx-2" onClick={handleclrclick}>clear</button>
+     <p> </p>
+   </div>
+
+    <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
+      <h2>your text summary</h2>
+      <p>{text.split(" ").length-1} words and {text.length}characters</p>
+      <h2>preview</h2>
+      <p>{text.length>0?text:"enter something in the textbox to preview it"}</p>
+    </div>
+   </>
+  )
+}
